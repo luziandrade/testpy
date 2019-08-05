20cfb70d14f559c77cba9b4160b9040adbdf5f89
Luana Andrade <luzi015@hotmail.com>
2019-08-05 15:32:50 +0100

    Initial commit

--- first_py.py
+++ first_py.py
@@ -39,8 +39,9 @@ def even_number_of_events(numbers):
     if evens == 0:
         return False
     else:
-        #return evens % 2 == 0
+        return evens % 2 ==
 
+#test
 
 assert even_number_of_events([]) == False, "No numbers"
 assert even_number_of_events([2]) == False, "One even number"
