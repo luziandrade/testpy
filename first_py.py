cc5fd3d8a336a2a51b76108377f20f7cee4611ac
Luana Andrade <luzi015@hotmail.com>
2019-08-05 15:42:22 +0100

    second commit

--- first_py.py
+++ first_py.py
@@ -41,7 +41,7 @@ def even_number_of_events(numbers):
     else:
         return evens % 2 ==
 
-#test
+#test2
 
 assert even_number_of_events([]) == False, "No numbers"
 assert even_number_of_events([2]) == False, "One even number"
